0f788bd562de2caf91effc978f25bf98802e801c
akram30-id <afalerin2016@gmail.com>
2025-01-15 11:45:28 +0700

    update: email length validation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { logger } from "./src/application/logging.js";
 import { web } from "./src/application/web.js";
 
-web.listen(300, () => {
+web.listen(3000, '0.0.0.0', () => {
     logger.info('App Started');
 })
